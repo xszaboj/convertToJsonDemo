950ff611b7dd9900e0678379182a5d4d22438bee
Jack <xszaboj@gmail.com>
2016-06-20 10:26:53 +0100

    init

---
+++ ConvertToJsonDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToJsonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter c = new Converter();
+            Console.WriteLine(c.NewsById());
+            Console.ReadLine();
+        }
+    }
+}
