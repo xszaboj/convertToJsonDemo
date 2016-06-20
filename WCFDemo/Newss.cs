e56fc8aa3ce07f13346ddd3b65b2486d0156b046
Jack <xszaboj@gmail.com>
2016-06-20 13:01:35 +0100

    update

---
+++ WCFDemo/Newss.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCFDemo
+{
+    public class Newss
+    {
+        public int CompanyId { get; set; }
+
+        public string CompanyLegalName { get; set; }
+
+        public int NewsCompId { get; set; }
+
+        public int NewsID { get; set; }
+    }
+}
---
+++ WCFDemo/Service1.svc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ConvertToJsonDemo;
+
+namespace WCFDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(string value)
+        {
+            Converter c = new Converter();
+            return c.NewsById();
+        }
+
+        public string GetDataWrapped(string value)
+        {
+            Converter c = new Converter();
+            return c.NewsById();
+        }
+
+        public System.Collections.Generic.List<Newss> GetList(string value)
+        {
+            Converter c = new Converter();
+            return c.NewsList();
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+
+
+
+
+        
+    }
+}
